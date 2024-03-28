e1f6dea30bd6e9bf7f288f74acecb1818d0985a8
Geurinckx Fabrice <f.geurinckx@stag.technofuturtic.education>
2024-03-28 14:35:20 +0100

    Version2

--- distributeur.py
+++ distributeur.py
@@ -1,44 +1,38 @@
-"""Algorithme Exo09 - Distributeur
+"""Algorithme Distributeur de boissons
 
-Variable    stockSoda, stockOrangeade, stockEau, choix : Entier
+Variable stock1, stock2, stock3, choix : Entier
 
 DEBUT
-    
-    stockSoda <- 3
-    stockOrangeade <- 5
-    stockEau <- 0
-
-    Ecrire("Veuillez sélectionner une boisson :")
-    Ecrire("1. Soda")
-    Ecrire("2. Orangeade")
-    Ecrire("3. Eau")
-    Lire(choix)
-    SELONQUE choix VAUT
-        1 : SI stockSoda > 0 ALORS
-                Ecrire("A votre santé, voici votre Soda!")
-                stockSoda <- stockSoda - 1
+    stock1 <- 0
+    stock2 <- 2
+    stock3 <- 3
+    FAIRE
+        Ecrire("Choisissez une boisson. Boisson1: 1, Boisson2: 2, Boisson3: 3, FINIR : 4")
+        Lire(choix)
+        SI choix == 1 ALORS
+            SI stock1 > 0 ALORS
+                Ecrire("Voici votre boisson1 , santé!")
+                stock1 <- stock1 - 1
             SINON
-                Ecrire("Sold out! Faîtes un autre choix...")
+                Ecrire("Plus de boisson1")
             FINSI
-        
-        2 : SI stockOrangeade > 0 ALORS
-                Ecrire("A votre santé, voici votre orangeade!")
-                stockOrangeade <- stockOrangeade - 1
+        SINONSI choix == 2 ALORS
+            SI stock2 > 0 ALORS
+                Ecrire("Voici votre boisson2 , santé!")
+                stock2 <- stock2 - 1
             SINON
-                Ecrire("Sold out! Faîtes un autre choix...")
+                Ecrire("Plus de boisson2")
             FINSI
-        3 :  SI stockEau > 0 ALORS
-                Ecrire("A votre santé, voici votre eau!")
-                stockEau <- stockEau - 1
+        SINONSI choix == 3 ALORS
+            SI stock3 > 0 ALORS
+                Ecrire("Voici votre boisson3 , santé!")
+                stock3 <- stock3 - 1
             SINON
-                Ecrire("Sold out! Faîtes un autre choix...")
+                Ecrire("Plus de boisson3")
             FINSI
-        SINON : Ecrire("Erreur, boisson non-disponible...")
-    FINSQ
-    
-    
-
-
+        FINSI        
+    TANTQUE ((stock1 + stock 2 + stock 3) > 0) ET (choix != 4) FINTQ
+    Ecrire("Merci d'utiliser distributeur 3000!")
 FIN
 """
 
@@ -46,25 +40,30 @@ stockSoda = 3
 stockOrangeade = 5
 stockEau = 0
 
-choix = print(input("Veuillez sélection une boisson : \n1. Soda\n2.Orangeade\n3. Eau"))
-match choix:
-    case "Soda":
-        if stockSoda > 0:
-            print("A votre santé, voici votre Soda!")
-            stockSoda = stockSoda - 1
-        else:
-            print("Sold out! Faîtes un autre choix")
-    case "Orangeande":
-        if stockOrangeade > 0:
-            print("A votre santé, voici votre orangeade!")
-            stockOrangeade = stockOrangeade - 1
-        else:
-            print("Sold out! Faîtes un autre choix")
-    case "Eau":
-        if stockEau > 0:
-            print("A votre santé, voici votre eau!")
-            stockEau = stockEau - 1
-        else:
-            print("Sold out! Faîtes un autre choix")
-    case _:
-        print("Erreur, boisson non disponible...")
+choix = 0
+
+while (stockSoda + stockOrangeade + stockEau) > 0 and choix != 4:
+    choix = print(int(input("Veuillez sélection une boisson : \n1. Soda\n2.Orangeade\n3. Eau\n 4. Finir")))
+    match choix:
+        case 1:
+            if stockSoda > 0:
+                print("A votre santé, voici votre Soda!")
+                stockSoda = stockSoda - 1
+            else:
+                print("Sold out! Faîtes un autre choix")
+        case 2:
+            if stockOrangeade > 0:
+                print("A votre santé, voici votre orangeade!")
+                stockOrangeade = stockOrangeade - 1
+            else:
+                print("Sold out! Faîtes un autre choix")
+        case 3:
+            if stockEau > 0:
+                print("A votre santé, voici votre eau!")
+                stockEau = stockEau - 1
+            else:
+                print("Sold out! Faîtes un autre choix")
+        case 4:
+            print("Merci d'utiliser distributeur 3000!")
+        case _:
+            print("Erreur, boisson non disponible...")
