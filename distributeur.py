2cabc1ec1df0e0261695740b5763f08d4dc5db93
Geurinckx Fabrice <f.geurinckx@stag.technofuturtic.education>
2024-03-29 09:46:22 +0100

    Version3

--- distributeur.py
+++ distributeur.py
@@ -1,69 +1,30 @@
-"""Algorithme Distributeur de boissons
+LIMITE_MAX = 3
 
-Variable stock1, stock2, stock3, choix : Entier
+stock = []
 
-DEBUT
-    stock1 <- 0
-    stock2 <- 2
-    stock3 <- 3
-    FAIRE
-        Ecrire("Choisissez une boisson. Boisson1: 1, Boisson2: 2, Boisson3: 3, FINIR : 4")
-        Lire(choix)
-        SI choix == 1 ALORS
-            SI stock1 > 0 ALORS
-                Ecrire("Voici votre boisson1 , santé!")
-                stock1 <- stock1 - 1
-            SINON
-                Ecrire("Plus de boisson1")
-            FINSI
-        SINONSI choix == 2 ALORS
-            SI stock2 > 0 ALORS
-                Ecrire("Voici votre boisson2 , santé!")
-                stock2 <- stock2 - 1
-            SINON
-                Ecrire("Plus de boisson2")
-            FINSI
-        SINONSI choix == 3 ALORS
-            SI stock3 > 0 ALORS
-                Ecrire("Voici votre boisson3 , santé!")
-                stock3 <- stock3 - 1
-            SINON
-                Ecrire("Plus de boisson3")
-            FINSI
-        FINSI        
-    TANTQUE ((stock1 + stock 2 + stock 3) > 0) ET (choix != 4) FINTQ
-    Ecrire("Merci d'utiliser distributeur 3000!")
-FIN
-"""
+stock.append(3)
+stock.append(0)
+stock.append(0)
 
-stockSoda = 3
-stockOrangeade = 5
-stockEau = 0
+nom = ["Eau", "Soda", "Orangeade"]
+
+totalstock = sum(stock)
 
 choix = 0
 
-while (stockSoda + stockOrangeade + stockEau) > 0 and choix != 4:
-    choix = print(int(input("Veuillez sélection une boisson : \n1. Soda\n2.Orangeade\n3. Eau\n 4. Finir")))
-    match choix:
-        case 1:
-            if stockSoda > 0:
-                print("A votre santé, voici votre Soda!")
-                stockSoda = stockSoda - 1
-            else:
-                print("Sold out! Faîtes un autre choix")
-        case 2:
-            if stockOrangeade > 0:
-                print("A votre santé, voici votre orangeade!")
-                stockOrangeade = stockOrangeade - 1
-            else:
-                print("Sold out! Faîtes un autre choix")
-        case 3:
-            if stockEau > 0:
-                print("A votre santé, voici votre eau!")
-                stockEau = stockEau - 1
-            else:
-                print("Sold out! Faîtes un autre choix")
-        case 4:
-            print("Merci d'utiliser distributeur 3000!")
-        case _:
-            print("Erreur, boisson non disponible...")
+while totalstock > 0 and choix != 4:
+    print("Veuillez sélection une boisson :")
+    for i in range(LIMITE_MAX):
+        print(f"{i+i}. {nom[i]}")
+    print("4. Finir")
+
+    choix = int(input())
+    
+    if choix != 4 :    
+        if (stock[choix-1] > 0):
+            print("A votre santé, voici votre Soda!")
+            stock[choix-1] = stock[choix-1] - 1
+        else:
+            print("Sold out! Faîtes un autre choix")
+    totalstock = sum(stock)
+print("Merci d'utiliser distributeur 3000!")
